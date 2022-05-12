25c99b546cc234a78e7cd6261eb6a04fdeb80ed2
MuffinDealer <55846965+Muffin-Dealer@users.noreply.github.com>
2022-05-12 23:22:54 +0300

    death
    game over screen

--- Why not balls/Assets/Scripts/Player/PlayerCollision.cs
+++ Why not balls/Assets/Scripts/Player/PlayerCollision.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
+    public GameOverScreen GameOverScreen;
+    public int score = 11;
+    private Rigidbody2D rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Lava")
+        if (collision.gameObject.CompareTag("Lava"))
         {
-            Destroy(gameObject);
+            Die();
+            //Destroy(gameObject);
         }
+    }
+    private void Die()
+    {
+        Time.timeScale = 0;
+        gameObject.SetActive(false);
+        GameOverScreen.Setup(score);
     }
 
 }
