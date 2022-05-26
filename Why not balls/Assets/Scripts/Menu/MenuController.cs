e18d44e1ca3a35c4067b511b2a2f97b15453ee96
Eimantas Šerpetis <eimantas.serpetis@ktu.edu>
2022-05-26 05:35:11 +0300

    Add options menu with audio volume option

--- Why not balls/Assets/Scripts/Menu/MenuController.cs
+++ Why not balls/Assets/Scripts/Menu/MenuController.cs
@@ -5,9 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+
     public void PlayGame()
     {
         SceneManager.LoadScene("SampleScene");
+    }
+
+    public void OpenOptions()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+
+    }
+
+    public void BackToMain()
+    {
+        optionsMenu.SetActive(false);
+        mainMenu.SetActive(true);
     }
 
     public void PlayHoverSound()
