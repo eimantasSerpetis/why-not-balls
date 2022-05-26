bf80c8b2a40216da02ba61fc3c93a86c68363a7e
prasauNereikia <luk.rdrm@gmail.com>
2022-05-26 12:54:39 +0300

    Add infinite amount of balls spawning

--- Why not balls/Assets/Scripts/Spawner/BallSpawner.cs
+++ Why not balls/Assets/Scripts/Spawner/BallSpawner.cs
@@ -10,120 +10,188 @@
     public GameObject quad; //Area in which balls can spawn
     public float ballCheckRadius; //Radius for collision checking
     public int maxSpawnAttemptsPerBall = 10; //Max attempts to spawn a ball to evade infinite loop
+    public static int scoreBallCount; //Count of score balls that are existing in level
+    public bool firstSpawn = true; //If true spawn both score and spike balls if false spawns only score balls and outside camera view
+    public GameObject scoreBall;
  
     void Awake()
+    {
+        scoreBallCount = 0;
+    }
+    void Update()
     {
         GameObject Ball; //Ball object
         MeshCollider c = quad.GetComponent<MeshCollider>();
         float screenX, screenY; //Coordinates X and Y
 
-        for (int i = 0; i < balls.Count; i++)
+        if(firstSpawn)
         {
-            Ball = balls[i];
-            switch(Ball.tag)
+            firstSpawn = false;
+            for (int i = 0; i < balls.Count; i++)
             {
-                case "ScoreBall":
-                    for (int j = 0; j < scoreBallsToPlace; j++)
-                    {
-                        // Create a position variable
-                        Vector2 position = Vector2.zero;
-            
-                        // whether or not we can spawn in this position
-                        bool validPosition = false;
-            
-                        // How many times we've attempted to spawn this ball
-                        int spawnAttempts = 0;
-            
-                        // While we don't have a valid position 
-                        //  and we haven't tried spawning this obstable too many times
-                        while(!validPosition && spawnAttempts < maxSpawnAttemptsPerBall)
+                Ball = balls[i];
+                switch(Ball.tag)
+                {
+                    case "ScoreBall":
+                        scoreBall = Ball;
+                        for (int j = 0; j < scoreBallsToPlace; j++)
                         {
-                            // Increase our spawn attempts
-                            spawnAttempts++;
-
-                            //Assign random position
-                            screenX = Random.Range(c.bounds.min.x, c.bounds.max.x);
-                            screenY = Random.Range(c.bounds.min.y, c.bounds.max.y);
-                            position = new Vector2(screenX, screenY);
-
-                            // This position is valid until proven invalid
-                            validPosition = true;
-            
-                            // Collect all colliders within our Ball Check Radius
-                            Collider2D[] colliders = Physics2D.OverlapCircleAll(position, ballCheckRadius);
-            
-                            // Go through each collider collected
-                            foreach(Collider2D col in colliders)
+                            // Create a position variable
+                            Vector2 position = Vector2.zero;
+                
+                            // whether or not we can spawn in this position
+                            bool validPosition = false;
+                
+                            // How many times we've attempted to spawn this ball
+                            int spawnAttempts = 0;
+                
+                            // While we don't have a valid position 
+                            //  and we haven't tried spawning this obstable too many times
+                            while(!validPosition && spawnAttempts < maxSpawnAttemptsPerBall)
                             {
-                                // If this collider is tagged "SpikeBall" or "ScoreBall" or "StartZone"
-                                if(col.tag == "SpikeBall" || col.tag == "ScoreBall" || col.tag == "StartZone")
+                                // Increase our spawn attempts
+                                spawnAttempts++;
+
+                                //Assign random position
+                                screenX = Random.Range(c.bounds.min.x, c.bounds.max.x);
+                                screenY = Random.Range(c.bounds.min.y, c.bounds.max.y);
+                                position = new Vector2(screenX, screenY);
+
+                                // This position is valid until proven invalid
+                                validPosition = true;
+                
+                                // Collect all colliders within our Ball Check Radius
+                                Collider2D[] colliders = Physics2D.OverlapCircleAll(position, ballCheckRadius);
+                
+                                // Go through each collider collected
+                                foreach(Collider2D col in colliders)
                                 {
-                                    // Then this position is not a valid spawn position
-                                    validPosition = false;
+                                    // If this collider is tagged "SpikeBall" or "ScoreBall" or "StartZone"
+                                    if(col.tag == "SpikeBall" || col.tag == "ScoreBall" || col.tag == "StartZone")
+                                    {
+                                        // Then this position is not a valid spawn position
+                                        validPosition = false;
+                                    }
                                 }
                             }
-                        }
 
-                        // If we exited the loop with a valid position
-                        if(validPosition)
-                        {
-                            // Spawn the ball here
-                            Instantiate(Ball, position, Quaternion.identity);
-                        }
-                    }
-                    break;
-                case "SpikeBall":
-                    for (int j = 0; j < spikeBallsToPlace; j++)
-                    {
-                        // Create a position variable
-                        Vector2 position = Vector2.zero;
-            
-                        // whether or not we can spawn in this position
-                        bool validPosition = false;
-            
-                        // How many times we've attempted to spawn this ball
-                        int spawnAttempts = 0;
-            
-                        // While we don't have a valid position 
-                        //  and we haven't tried spawning this obstable too many times
-                        while(!validPosition && spawnAttempts < maxSpawnAttemptsPerBall)
-                        {
-                            // Increase our spawn attempts
-                            spawnAttempts++;
-
-                            //Assign random position
-                            screenX = Random.Range(c.bounds.min.x, c.bounds.max.x);
-                            screenY = Random.Range(c.bounds.min.y, c.bounds.max.y);
-                            position = new Vector2(screenX, screenY);
-
-                            // This position is valid until proven invalid
-                            validPosition = true;
-            
-                            // Collect all colliders within our Ball Check Radius
-                            Collider2D[] colliders = Physics2D.OverlapCircleAll(position, ballCheckRadius);
-            
-                            // Go through each collider collected
-                            foreach(Collider2D col in colliders)
+                            // If we exited the loop with a valid position
+                            if(validPosition)
                             {
-                                // If this collider is tagged "SpikeBall" or "ScoreBall" or "StartZone"
-                                if(col.tag == "SpikeBall" || col.tag == "ScoreBall" || col.tag == "StartZone")
-                                {
-                                    // Then this position is not a valid spawn position
-                                    validPosition = false;
-                                }
+                                // Spawn the ball here
+                                Instantiate(Ball, position, Quaternion.identity);
+                                scoreBallCount += 1;
                             }
                         }
-            
-                        // If we exited the loop with a valid position
-                        if(validPosition)
+                        break;
+                    case "SpikeBall":
+                        for (int j = 0; j < spikeBallsToPlace; j++)
                         {
-                            // Spawn the ball here
-                            Instantiate(Ball, position, Quaternion.identity);
+                            // Create a position variable
+                            Vector2 position = Vector2.zero;
+                
+                            // whether or not we can spawn in this position
+                            bool validPosition = false;
+                
+                            // How many times we've attempted to spawn this ball
+                            int spawnAttempts = 0;
+                
+                            // While we don't have a valid position 
+                            //  and we haven't tried spawning this obstable too many times
+                            while(!validPosition && spawnAttempts < maxSpawnAttemptsPerBall)
+                            {
+                                // Increase our spawn attempts
+                                spawnAttempts++;
+
+                                //Assign random position
+                                screenX = Random.Range(c.bounds.min.x, c.bounds.max.x);
+                                screenY = Random.Range(c.bounds.min.y, c.bounds.max.y);
+                                position = new Vector2(screenX, screenY);
+
+                                // This position is valid until proven invalid
+                                validPosition = true;
+                
+                                // Collect all colliders within our Ball Check Radius
+                                Collider2D[] colliders = Physics2D.OverlapCircleAll(position, ballCheckRadius);
+                
+                                // Go through each collider collected
+                                foreach(Collider2D col in colliders)
+                                {
+                                    // If this collider is tagged "SpikeBall" or "ScoreBall" or "StartZone"
+                                    if(col.tag == "SpikeBall" || col.tag == "ScoreBall" || col.tag == "StartZone")
+                                    {
+                                        // Then this position is not a valid spawn position
+                                        validPosition = false;
+                                    }
+                                }
+                            }
+                
+                            // If we exited the loop with a valid position
+                            if(validPosition)
+                            {
+                                // Spawn the ball here
+                                Instantiate(Ball, position, Quaternion.identity);
+                            }
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        else
+        {
+            if(scoreBallCount <= scoreBallsToPlace / 5)
+            {
+                for (int j = 0; j < scoreBallsToPlace - scoreBallsToPlace / 5; j++)
+                {
+                    // Create a position variable
+                    Vector2 position = Vector2.zero;
+        
+                    // whether or not we can spawn in this position
+                    bool validPosition = false;
+        
+                    // How many times we've attempted to spawn this ball
+                    int spawnAttempts = 0;
+        
+                    // While we don't have a valid position 
+                    //  and we haven't tried spawning this obstable too many times
+                    while(!validPosition && spawnAttempts < maxSpawnAttemptsPerBall)
+                    {
+                        // Increase our spawn attempts
+                        spawnAttempts++;
+
+                        //Assign random position
+                        screenX = Random.Range(c.bounds.min.x, c.bounds.max.x);
+                        screenY = Random.Range(c.bounds.min.y, c.bounds.max.y);
+                        position = new Vector2(screenX, screenY);
+
+                        // This position is valid until proven invalid
+                        validPosition = true;
+        
+                        // Collect all colliders within our Ball Check Radius
+                        Collider2D[] colliders = Physics2D.OverlapCircleAll(position, ballCheckRadius);
+        
+                        // Go through each collider collected
+                        foreach(Collider2D col in colliders)
+                        {
+                            // If this collider is tagged "SpikeBall" or "ScoreBall" or "StartZone"
+                            if(col.tag == "SpikeBall" || col.tag == "ScoreBall" || col.tag == "StartZone" || col.tag == "MainCamera")
+                            {
+                                // Then this position is not a valid spawn position
+                                validPosition = false;
+                            }
                         }
                     }
-                    break;
-                default:
-                    break;
+
+                    // If we exited the loop with a valid position
+                    if(validPosition)
+                    {
+                        // Spawn the ball here
+                        Instantiate(scoreBall, position, Quaternion.identity);
+                        scoreBallCount += 1;
+                    }
+                }
             }
         }
     }
