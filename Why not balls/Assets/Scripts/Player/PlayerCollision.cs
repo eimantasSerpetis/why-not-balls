55d5f1abdeecfb8f4df01f0525928cb3b90be2b7
prasauNereikia <luk.rdrm@gmail.com>
2022-05-26 13:43:41 +0300

    Update spawner, update level

--- Why not balls/Assets/Scripts/Player/PlayerCollision.cs
+++ Why not balls/Assets/Scripts/Player/PlayerCollision.cs
@@ -40,6 +40,7 @@ public class PlayerCollision : MonoBehaviour
                 ScoreController.scoreValue += 1;
                 Instantiate(scoreBallExplosion, collider.gameObject.transform.position, collider.gameObject.transform.rotation);
                 Destroy(collider.gameObject);
+                BallSpawner.scoreBallCount -= 1;
                 Timer.restartTimer();  
                 movementController.RedirectUpwards();
                 movementController.makeControllable();
